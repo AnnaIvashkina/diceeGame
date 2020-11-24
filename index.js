0303009ba72df26d7fd3c6f1f6c1dedb118024ed
Anna Ivashkina <69637786+AnnaIvashkina@users.noreply.github.com>
2020-11-24 12:58:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.ceil(Math.random()*6);
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").textContent = "🚩Player 1 Wins!"
+} else if (randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins 🚩!"
+} else {
+  document.querySelector("h1").textContent = "Draw!"
+}
